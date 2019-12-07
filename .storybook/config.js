45db62aaac8b847896f3454518f1c4c876f047c5
Austin Malerba <austin.malerba@gmail.com>
2019-12-06 19:12:33 -0600

    init

---
+++ .storybook/config.js
@@ -0,0 +1,5 @@
+import { configure } from '@storybook/react';
+
+// automatically import all files ending in *.stories.js
+configure(require.context('../stories', true, /\.stories\.tsx?$/), module)
+
---
+++ stories/1-Button.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ExampleComponent from '../dist'
+
+export default {
+  title: 'test',
+};
+
+export const emoji = () => (
+  <ExampleComponent text="hi" />
+);
