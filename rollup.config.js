994a21d32d2c9eb10436051ca005cfa74fe06fc2
Austin Malerba <austin.malerba@gmail.com>
2019-12-12 08:08:56 -0600

    readme updates

--- rollup.config.js
+++ rollup.config.js
@@ -10,7 +10,7 @@ import svgr from '@svgr/rollup'
 import pkg from './package.json'
 
 export default {
-  input: 'src/index.tsx',
+  input: 'src/index.ts',
   output: [
     {
       file: pkg.main,
